c5c6efdff2e0d278ade93c29d1481ced1977bcca
Clark Alms <clark.alms@gmail.com>
2021-06-21 12:11:14 -0400

    added basic grid/row/cell makers

--- cellular_automata/src/App.js
+++ cellular_automata/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let config = {
+		x: 10,
+		y: 10,
+	};
+
+	const gridMaker = (x, y) => {
+		let out = '';
+		for (let iy = y - 1; iy >= 0; iy--) {
+			let cells = '';
+			for (let ix = x - 1; ix >= 0; ix--) {
+				let className = `x${ix}y${iy}`;
+				cells += cellMaker(className);
+			}
+			out += rowMaker(cells);
+		}
+		return out;
+	};
+
+	function rowMaker(cells) {
+		return `<div className="cell-row">${cells}</div>`;
+	}
+
+	function cellMaker(className) {
+		return `<div className="cell${className}">.</div>`;
+	}
+
+	if (document.getElementsByClassName('grid')[0]) {
+		const initialGrid = gridMaker(config.x, config.y);
+		document.getElementsByClassName('grid')[0].innerHTML = initialGrid;
+		document.getElementsByClassName('next')[0].innerHTML = initialGrid;
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div className="grid"></div>
+				<div className="next"></div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
